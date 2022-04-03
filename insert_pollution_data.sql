4a8e8273c1f29e6a767b1a62a183d0539cb67830
Nick Madaffari <79033193+nick-madaffari@users.noreply.github.com>
2022-04-03 13:57:39 -0500

    Updated data types for efficiency

--- insert_pollution_data.sql
+++ insert_pollution_data.sql
@@ -4,8 +4,8 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_pollution_data`(
     country VARCHAR(45),
     state VARCHAR(45),
     city VARCHAR(45),
-    us_aqi INT,
-    cn_aqi INT
+    us_aqi SMALLINT,
+    cn_aqi SMALLINT
 )
 BEGIN
 	INSERT INTO pollution
